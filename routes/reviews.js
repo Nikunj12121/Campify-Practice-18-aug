2206aecf933a214b92057b26257971ac069c95c8
Nikunj Singla <nikunjsingla2005@gmail.com>
2025-08-20 19:55:45 +0530

    controllersdone

--- routes/reviews.js
+++ routes/reviews.js
@@ -7,34 +7,12 @@ const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const { model } = require('mongoose');
 const { validateReview,isLoggedIn,isReviewAuthor } = require('../middleware');
+const reviews = require('../controllers/reviews');
 
 
 
+router.post("/",isLoggedIn,validateReview, catchAsync(reviews.createReview));
 
-router.post("/",isLoggedIn,validateReview, catchAsync(async (req, res) => {
-
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id; // Set the author to the logged-in user
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', 'Successfully created a new review!');
-    res.redirect(`/campgrounds/${campground._id}`);
-
-
-}));
-
-router.delete('/:reviewId',isLoggedIn,isReviewAuthor,catchAsync(async (req, res) => {
-
-    const {id,reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: {reviews: reviewId} });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success', 'Successfully deleted review!');
-    res.redirect(`/campgrounds/${id}`);
-
-    
-
-}));
+router.delete('/:reviewId',isLoggedIn,isReviewAuthor,catchAsync(reviews.deleteReview));
 
 module.exports = router;
