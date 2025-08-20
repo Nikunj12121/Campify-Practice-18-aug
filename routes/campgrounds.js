90382545fbe81f841ecbd397f610eb6ffe9a78b8
Nikunj Singla <nikunjsingla2005@gmail.com>
2025-08-20 15:29:05 +0530

    51half_done

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Campground = require('../models/campground');
+const { isLoggedIn } = require('../middleware');
 
 const { campgroundSchema } = require('../schemas.js');
 
@@ -15,8 +16,8 @@ router.get('/', async (req, res) => {
     res.render('campgrounds/index', { campgrounds });
 });
 
-router.get('/new', (req, res) => {
-
+router.get('/new', isLoggedIn,(req, res) => {
+ 
     res.render('campgrounds/new');
 
 });
@@ -37,7 +38,7 @@ const validateCampground = (req, res, next) => {
 
 
 
-router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+router.post('/',isLoggedIn ,validateCampground, catchAsync(async (req, res, next) => {
     // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
@@ -46,7 +47,7 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 }))
 
 
-router.get('/:id', catchAsync(async (req, res) => {
+router.get('/:id',catchAsync(async (req, res) => {
 
     const campground = await Campground.findById(req.params.id).populate('reviews');
 
@@ -59,7 +60,7 @@ router.get('/:id', catchAsync(async (req, res) => {
 
 }));
 
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn,catchAsync(async (req, res) => {
 
         const campground = await Campground.findById(req.params.id);
         res.render('campgrounds/edit',{campground});
@@ -68,7 +69,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 }));
 
 
-router.put('/:id',validateCampground, catchAsync(async (req, res) => {
+router.put('/:id',isLoggedIn,validateCampground, catchAsync(async (req, res) => {
 
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
